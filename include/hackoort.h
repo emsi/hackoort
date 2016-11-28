28953ad38b08e474b315d1505df95aab94c7bf62
Mariusz Woloszyn <emsi@users.noreply.github.com>
2016-11-28 19:24:53 +0000

    Added context pointer

--- include/hackoort.h
+++ include/hackoort.h
@@ -1,3 +1,23 @@
+/* liboort.h
+ *
+ * Copyright (C) 2016 Mariusz Woloszyn <emsi@nosuchdomain.example>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#ifndef __LIBOORT_H__
+#define __LIBOORT_H__
 
 typedef struct _hackoortContext hackoortContext;
 
@@ -8,6 +28,7 @@ struct _hackoortContext {
     // configuration
     char* bt_address;  // remote device address
     char* password; // default password used when no password is set
+    void* context; // pointer to be passed around
 };
 
 
@@ -23,3 +44,5 @@ int hackoort_set_temperature(hackoortContext* context, unsigned char lum);
 int hackoort_set_temperature_pct(hackoortContext* context, unsigned char pct);
 int hackoort_set_rgb(hackoortContext* context, char r, char g, char b);
 int hackoort_set_rgb_onoff(hackoortContext* context, unsigned on);
+
+#endif /* __LIBOORT_H__ */
