653f98a7dbd389625140d1a57aba85cf8d62692b
Mariusz Woloszyn <emsi@users.noreply.github.com>
2019-06-04 20:33:37 +0200

    Setup configurable via example yaml file.

--- python/oorthap/__main__.py
+++ python/oorthap/__main__.py
@@ -1,10 +1,3 @@
-"""An example of how to setup and start an Accessory.
-
-This is:
-1. Create the Accessory object you want.
-2. Add it to an AccessoryDriver, which will advertise it on the local network,
-    setup a server to answer client queries, etc.
-"""
 import logging
 import signal
 
@@ -16,20 +9,52 @@ from oorthap.bulb import OortColorBulb, OortColorBulbSwitch
 
 logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
 
+OORT_CONFIG = "/etc/oort/oort.yaml"
+
+
+def load_config():
+    from ruamel import yaml
+
+    with open(OORT_CONFIG, 'r') as fp:
+        yaml = yaml.YAML(typ='safe')
+        config = yaml.load(fp)
+
+    config['verbose'] = config.get('verbose') or 0
+    config['port'] = config.get('port') or 51826
+    config['bridge_name'] = config.get('bridge_name') or 'oort'
+
+    return config
+
 
 def get_bridge(driver):
     """Call this method to get a Bridge instead of a standalone accessory."""
-    bridge = Bridge(driver, 'pyBridge')
+    bridge = Bridge(driver, config['bridge_name'])
     bridge.set_info_service(manufacturer="Emsi")
 
-    bulb = Bulb('84:EB:18:7D:F8:61'.encode(), verbose=1).connect()
-    bridge.add_accessory(OortColorBulb(driver, 'Corner', bulb))
-    bridge.add_accessory(OortColorBulbSwitch(driver, 'Color switch', bulb))
+    devices = config['devices']
+    for device in devices:
+        password = devices[device].get('password') or b'D000000'
+        bulb = Bulb(
+            devices[device]['address'].encode(), password=password,
+            verbose=config['verbose']
+        ).connect()
+        name = devices[device].get('name') or device
+        bridge.add_accessory(OortColorBulb(driver, name, bulb))
+        if devices[device]['color_switch'] == 'yes':
+            bridge.add_accessory(OortColorBulbSwitch(
+                driver, '{} color switch'.format(device), bulb
+            ))
     return bridge
 
 
+config = load_config()
+print(config)
+
 # Start the accessory on port 51826
-driver = AccessoryDriver(port=51826, pincode=b"666-77-666")
+driver = AccessoryDriver(
+    port=config['port'],
+    persist_file='/etc/oort/accessory.state'
+)
 
 # Change `get_accessory` to `get_bridge` if you want to run a Bridge.
 driver.add_accessory(accessory=get_bridge(driver))
